e26864d2943f0ed5c9e0fd9f13ec2640b5f432ae
Charles Peden and Dan Lauby <dan@>
2017-01-23 10:30:49 -0800

    build tasks constructor

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function Tasks (taskDescription, taskDueDate, taskCompleted) {
+  this.taskDescription = taskDescription;
+  this.taskDueDate = taskDueDate;
+  this.taskCompleted = taskCompleted;
+};
