388e5ecb583108ebaf5134d344855aba0c8f7e79
Charles Peden and Dan Lauby <charles@>
2017-01-23 11:06:50 -0800

    add input variables to take values from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
+//back end logic
+
 function Tasks (taskDescription, taskDueDate, taskCompleted) {
   this.taskDescription = taskDescription;
   this.taskDueDate = taskDueDate;
   this.taskCompleted = taskCompleted;
 };
+
+
+//front end logic
+$(function(){
+  $("form#inputTask").submit(function(event){
+    event.preventDefault();
+    var inputtedTaskDescription = $("input#taskDescription").val();
+    var inputtedtaskDueDate = $("input#taskDueDate").val();
+
+    var newTask = new Tasks(inputtedTaskDescription, inputtedtaskDueDate, false);
+
+    
+  });
+});
